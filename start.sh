c56c8a7136bd77f1f42fa8b943dfc69ccaff2158
RHamudi <ramonramos.silva16@gmail.com>
2025-02-07 07:26:03 -0300

    teste5

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@
 uvicorn app:app --host 0.0.0.0 --port $PORT &
 
 # Inicia o Celery worker
-# celery -A task worker -l info --pool=solo &
+celery -A task worker -l info --pool=solo &
 
 # Inicia o Celery Flower para monitoramento
-celery -A task flower --address=127.0.0.6 --port=$PORT &
+# celery -A task flower --address=127.0.0.6 --port=$PORT &
 
 # Aguarda processos para evitar que o script finalize
 wait
