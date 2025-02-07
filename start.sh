d93dd695ba7472ccc371c3eeff1164aa86c63f71
RHamudi <ramonramos.silva16@gmail.com>
2025-02-07 07:22:01 -0300

    teste deploy 4

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 uvicorn app:app --host 0.0.0.0 --port $PORT &
 
 # Inicia o Celery worker
-celery -A task worker -l info --pool=solo &
+# celery -A task worker -l info --pool=solo &
 
 # Inicia o Celery Flower para monitoramento
 celery -A task flower --address=127.0.0.6 --port=$PORT &
